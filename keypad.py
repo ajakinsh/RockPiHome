da9081b08dfaba09b103a15843a45f661e809604
ajakinsh <71792857+ajakinsh@users.noreply.github.com>
2023-04-14 01:58:29 +0000

    working and finished keypad
    Changed matrix object for each key to be detected properly

--- keypad.py
+++ keypad.py
@@ -1,27 +1,22 @@
 import mraa
 import time
 
-# Define the row and column pins
 row_pins = [37, 36, 38, 40]
 col_pins = [29, 31, 33, 35]
 
-# Initialize the row pins as input and the column pins as output
 for pin in row_pins:
     mraa.Gpio(pin).dir(mraa.DIR_IN)
 for pin in col_pins:
     mraa.Gpio(pin).dir(mraa.DIR_OUT)
 
-# Define the keypad matrix
 keypad = [
-    ['1', '2', '3', 'A'],
-    ['4', '5', '6', 'B'],
-    ['7', '8', '9', 'C'],
-    ['*', '0', '#', 'D']
+    ['1', '4', '7', '*'],
+    ['2', '5', '8', '0'],
+    ['3', '6', '9', '#'],
+    ['A', 'B', 'C', 'D']
 ]
 
-# Function to read the keypad and detect button presses
 def read_keypad():
-    while True:
         for i in range(len(col_pins)):
             # Set the current column pin to high
             mraa.Gpio(col_pins[i]).write(1)
@@ -35,5 +30,7 @@ def read_keypad():
             # Set the current column pin back to low
             mraa.Gpio(col_pins[i]).write(0)
 
-# Call the function to read the keypad
-read_keypad()
+print("Begin...")
+
+while True:
+    read_keypad()
