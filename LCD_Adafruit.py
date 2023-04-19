ae96b790031afe7e1f044b38708137084441fc47
ajakinsh <71792857+ajakinsh@users.noreply.github.com>
2023-04-19 17:28:16 -0400

    changed to newer adafruit lcd library

--- LCD_Adafruit.py
+++ LCD_Adafruit.py
@@ -1,27 +1,38 @@
-import Adafruit_CharLCD as LCD
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as characterlcd
 import time
 
-# Initialize the LCD using the pins of your choice
-lcd_rs = 27
-lcd_en = 22
-lcd_d4 = 25
-lcd_d5 = 24
-lcd_d6 = 23
-lcd_d7 = 18
+# Modify this if you have a different sized character LCD
 lcd_columns = 16
 lcd_rows = 2
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+# Define the LCD pins
+lcd_rs = digitalio.DigitalInOut(board.D2)
+lcd_en = digitalio.DigitalInOut(board.D4)
+lcd_d0 = digitalio.DigitalInOut(board.C6)
+lcd_d1 = digitalio.DigitalInOut(board.C5)
+lcd_d2 = digitalio.DigitalInOut(board.B0)
+lcd_d3 = digitalio.DigitalInOut(board.A7)
+lcd_d4 = digitalio.DigitalInOut(board.B1)
+lcd_d5 = digitalio.DigitalInOut(board.D5)
+lcd_d6 = digitalio.DigitalInOut(board.B2)
+lcd_d7 = digitalio.DigitalInOut(board.D7) #Need to change, pin 26 has no board GPIO name)
+
+# Initialize the LCD class
+lcd = characterlcd.Character_LCD_8bit(
+    lcd_rs, lcd_en, lcd_d0, lcd_d1, lcd_d2, lcd_d3, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows
+)
 
 # Write a message to the LCD
 lcd.clear()
-lcd.message('locked')
+lcd.message = "locked"
 time.sleep(2)
 
 # Clear the LCD and write a different message
-while True:
-    lcd.clear()
-    lcd.message('unlocked')
-    time.sleep(2)
+lcd.clear()
+lcd.message = "unlocked"
+time.sleep(2)
 
 # Clear the LCD when done
-# lcd.clear()
+lcd.clear()
